3aa319746e34a9ab5f5cb975ff58433010a1cbce
RealSwitzer <therealswitzerwicki@gmail.com>
2021-04-22 17:31:39 -0600

    Fix it as an executable (add env3 and fix encoder var)

--- rot13.py
+++ rot13.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 import codecs
 
 def infloop():
-    passrot13 = lambda s : codecs.getencoder("rot-13")(s)[0]
+    rot13 = lambda s : codecs.getencoder("rot-13")(s)[0]
     print("NOTE: Encoding a ROT13 string will decode it.")
     thestring = str(input("What string do you want to ROT13 encode? "))
     print(rot13(thestring))
