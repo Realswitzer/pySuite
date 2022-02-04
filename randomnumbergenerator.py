f85b35b833db625fe945fcfc3d60655cbf4b8ef2
Realswitzer <therealswitzerwicki@gmail.com>
2022-02-03 20:54:20 -0600

    control c doesnt work, ill fix it eventually

--- randomnumbergenerator.py
+++ randomnumbergenerator.py
@@ -1,16 +1,23 @@
 import random
 
 def infloop():
-    a = int(input("Enter the first number of the range that you want the number generator to generate: "))
-    b = int(input("Enter the second number of the range that you want the number generator to generate: "))
-
+    try:
+        a = int(input("Enter the first number of the range that you want the number generator to generate: "))
+    except:
+        print("Variable a attempted to be set to a non-integer character!")
+        print()
+        infloop()
+    try:
+        b = int(input("Enter the second number of the range that you want the number generator to generate: "))
+    except:
+        print("Variable b attempted to be set to a non-integer character!")
+        print()
+        infloop()
     print('Here is your random number from',a,'to',b,":",(random.randint(a, b)))
     print("Would you like to generate another random number? (y/n)\n")
     c = str(input())
-    print()
     if c == "y":
         infloop()
     elif c == "n":
         exit()
-
 infloop()
