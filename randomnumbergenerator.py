c4ec645fa2e8e0f9f6d2f1045025a206bf2a46a5
Realswitzer <therealswitzerwicki@gmail.com>
2022-02-03 19:44:09 -0600

    Staging for randomnumbergenerator, will finish later.

--- randomnumbergenerator.py
+++ randomnumbergenerator.py
@@ -1,18 +1,16 @@
 import random
-from random import randrange
 
-while True :
+def infloop():
     a = int(input("Enter the first number of the range that you want the number generator to generate: "))
     b = int(input("Enter the second number of the range that you want the number generator to generate: "))
-    c = 0
-    
-    while c < 1:
-        print('Here is your random number from',a,'to',b,":",(randrange(a, b)))
-        c += 1
 
-    print("Would you like to generate another random number? (y/n) (You might have to type it a few times for some reason, if you can fix it in the code, please do and submit a pull request. Thanks.)")
-    input()
-    if input() == "y":
-        False
-    if input() == "n":
-        break
+    print('Here is your random number from',a,'to',b,":",(random.randint(a, b)))
+    print("Would you like to generate another random number? (y/n)\n")
+    c = str(input())
+    print()
+    if c == "y":
+        infloop()
+    elif c == "n":
+        exit()
+
+infloop()
--- scripts.md
+++ scripts.md
@@ -1,4 +1,5 @@
-| Script          | Description    |
-| :-------------  | :------------- |
-| uniasciidecoder | Takes Unicode/Ascii code and converts into the letter (works with 33-127) |
-| rot13           | Encodes/decodes ROT13, works at least with the English alphabet, I wouldn't recommend other characters |
+| Script                | Description    |
+| :-------------------  | :------------- |
+| uniasciidecoder       | Takes Unicode/Ascii code and converts into the letter (works with 33-127) |
+| rot13                 | Encodes/decodes ROT13, works at least with the English alphabet, I wouldn't recommend other characters |
+| randomnumbergenerator | Generates a random number within a range. |
