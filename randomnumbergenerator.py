5c71e1dcf85dddc6ebf3bc198246b207887d5a5f
NightKoneko <62818210+NightKoneko@users.noreply.github.com>
2022-02-07 12:34:27 -0500

    Made the random number generator a lot better (Added GUI, cleaned up code, etc)
    You are welcome

--- randomnumbergenerator.py
+++ randomnumbergenerator.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python3
+
+import dearpygui.dearpygui as dpg
 import random
 
-def infloop():
-    a = input("Enter the first number of the range that you want the number generator to generate: ")
-    b = input("Enter the second number of the range that you want the number generator to generate: ")
-    try:
-        print('Here is your random number from',a,'to',b,":",(random.randint(int(a), int(b))))
-    except ValueError:
-        print("Variable(s) contained non-integers!\n")
-        infloop()
-    c = input(("Would you like to generate another random number? (y/n)\n"))
-    if c == ("y" or "yes"):
-        infloop()
-    elif c == ("n" or "no" or "exit" or "quit" or "q"):
-        exit()
+range1 = 0
+range2 = 0
 
-infloop()
+def rand_gen(s=None, data=None):
+	r = 0
+	try:
+		r = random.randint(range1, range2)
+	except Exception:
+		dpg.set_value("rand", "(invalid)")
+		return
+	dpg.set_value("rand", str(r))
+
+def rand_cb(s, data):
+	if not s in globals():
+		raise ValueError("invalid sender " + s)
+	globals()[s] = data
+	rand_gen()
+
+dpg.create_context()
+
+with dpg.window(tag="rng", label="Random Generator"):
+	with dpg.group(tag="rgroup", width=200, horizontal=True):
+		dpg.add_text("Range:")
+		dpg.add_input_int(tag="range1", callback=rand_cb)
+		dpg.add_text("-")
+		dpg.add_input_int(tag="range2", callback=rand_cb)
+	with dpg.group(tag="result", horizontal=True):
+		dpg.add_text("Output:")
+		dpg.add_text(tag="rand", default_value="(uninitialized)")
+	dpg.add_button(label="Regenerate", callback=rand_gen)
+
+dpg.create_viewport(width=640, height=480)
+dpg.setup_dearpygui()
+dpg.set_primary_window("rng", True)
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
