bccdafd18140972cbe88842bca8148d6c0fca6e1
RealSwitzer <therealswitzerwicki@gmail.com>
2022-05-13 06:05:52 -0500

    rename to allow PR

--- randnumgen.py
+++ randnumgen.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import random, sys, getopt
+import random, sys, getopt, platform
 
 def infloop():
     a = input("Enter the first number of the range that you want the number generator to generate: ")
@@ -23,21 +23,32 @@ def main(argv):
     a = ''
     b = ''
     try:
-        opts, args = getopt.getopt(argv,"hn:x:",["help","min=","max="])
+        opts, args = getopt.getopt(argv,"hn:x:",["help","min=","max=","headless"])
     except getopt.GetoptError:
         print("I don't know how you triggered the opt error, but here we are. Exiting")
         exit()
     if opts == []:
-        print('I assume you ran this with no arguments expecting the slow CLI method.\n')
-        infloop()
+        print("No arguments! Launching GUI version...because apparently #4 exists")
+        if platform.system() == 'Darwin':
+            print("Sorry, dearpygui doesn't seem to work on Mac, at least for me (developer).")
+            print("I hope that pygame works, but there's no implementation of that.")
+            print("Please use --headless (cli gui-thing) or --help for other options. -switz")
+            exit(1)
+        dpgfunc()
     for opt, arg in opts:
         if opt in ("-h", "--help"):
-            print('Usage: [file name] -n/--min <minimum value> -x/--max <maximum value>')
+            print('Usage: [file name] [args]')
+            print('args: ')
+            print('')
+            print('   -n --min <minimum value>')
+            print('   -x --max <maximum value>')
             exit()
         elif opt in ("-n", "--min"):
             a = str(arg)
         elif opt in ("-x", "--max"):
             b = str(arg)
+        elif opt == "--headless":
+            infloop()
     try:
         print('Here is your random number from',a,'to',b,':',(random.randint(int(a), int(b))))
     except ValueError:
